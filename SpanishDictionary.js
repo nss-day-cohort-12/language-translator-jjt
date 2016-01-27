d0866111c951f4dc4434fed63950e6ec69203784
Steven Tyler Roberts <steventylerroberts@Stevens-MacBook-Pro.local>
2016-01-27 14:26:53 -0600

    Setup SpanishDictionary, added object

---
+++ SpanishDictionary.js
@@ -0,0 +1,3 @@
+var Dictionary = (function(originalDictionary){
+	var spanishDictionary = {"merry": "feliz", "christmas": "navidad", "and": "y", "happy": "próspero", "new": "nuevo", "year": "año"};
+})(Dictionary);
