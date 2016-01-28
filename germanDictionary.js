4230a815c7020c2625b66ab0870b8139a09295bc
Jason Replogle <JasonReplogle@Jasons-MacBook-Pro-2.local>
2016-01-28 13:36:50 -0600

    German loop

--- germanDictionary.js
+++ germanDictionary.js
@@ -1,5 +1,24 @@
 
 var Dictionary = (function() {
   var germanDictionary = {"merry":"frohlich", "christmas":"weihnachten", "and":"neu", "happy":"glucklich", "new":"neu", "year":"jahr"};
-})();
 
+  return {
+
+  translateToGerman: function() {
+    var userStringToArray = userInputString.toLowerCase().split(" ");
+    console.log("userStringToArray", userStringToArray);
+    var keyArray = Object.keys(germanDictionary);
+    console.log("keyArray", keyArray);
+    for (var i = 0; i < userStringToArray.length; i++) {
+      if (keyArray.indexOf(userStringToArray[1]) !== -1) {
+        userStringToArray[i] = germanDictionary[userStringToArray[i]];
+      }
+    };
+    console.log("userStringToArray", userStringToArray);
+    var translatedString = userStringToArray.join(" ");
+
+    outputElement.innerHTML = translatedString;
+  }
+}
+  
+})();
