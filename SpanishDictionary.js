57eb2fd6f97603ec4f25587cad317683f9da205b
Tyler Roberts <steventylerfive@gmail.com>
2016-01-28 11:49:52 -0600

    Added translation function to spanish augment page

--- SpanishDictionary.js
+++ SpanishDictionary.js
@@ -1,3 +1,20 @@
 var Dictionary = (function(originalDictionary){
 	var spanishDictionary = {"merry": "feliz", "christmas": "navidad", "and": "y", "happy": "próspero", "new": "nuevo", "year": "año"};
+
+	originalDictionary.translateToSpanish = function() {
+		var userStringToArray = userInputString.toLowerCase().split(" ");
+		console.log("userStringToArray", userStringToArray);
+		var keyArray = Object.keys(spanishDictionary);
+		console.log("keyArray", keyArray);
+		for (var i = 0; i < userStringToArray.length; i++) {
+			if (keyArray.indexOf(userStringToArray[1]) !== -1) {
+				userStringToArray[i] = spanishDictionary[userStringToArray[i]];
+			}
+		};
+		console.log("userStringToArray", userStringToArray);
+		var translatedString = userStringToArray.join(" ");
+
+		outputElement.innerHTML = translatedString;
+	}
+	return originalDictionary;
 })(Dictionary);
